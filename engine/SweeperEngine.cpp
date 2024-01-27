928febc9897381b0ef55bf8a6dfbd5ae353795d9
ioannco <cheremisenov96@gmail.com>
2024-01-27 14:44:00 +0300

    Create engine class

---
+++ engine/SweeperEngine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chere on 27.01.2024.
+//
+
+#include "SweeperEngine.h"
---
+++ engine/SweeperEngine.h
@@ -0,0 +1,14 @@
+//
+// Created by chere on 27.01.2024.
+//
+
+#ifndef MINESWEEPER_SWEEPERENGINE_H
+#define MINESWEEPER_SWEEPERENGINE_H
+
+
+class SweeperEngine {
+
+};
+
+
+#endif //MINESWEEPER_SWEEPERENGINE_H
